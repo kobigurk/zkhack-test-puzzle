73d4f2ba1e88167b66a5ed5827bb940866abf33b
Kobi Gurkan <kobigurk@gmail.com>
2021-10-24 22:16:43 +0300

    feat: adds test puzzle

---
+++ src/bin/verify-test-puzzle.rs
@@ -0,0 +1,9 @@
+use test_puzzle::PUZZLE_DESCRIPTION;
+use prompt::{puzzle, welcome};
+
+fn main() {
+    welcome();
+    puzzle(PUZZLE_DESCRIPTION);
+
+    /* Your solution here! */
+}
---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+pub const PUZZLE_DESCRIPTION: &str = r#"Many have tried and never succeeded. Are you able to print your name?"#;
